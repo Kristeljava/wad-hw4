f86fbee42bc6907a7c221207e530cbdd94619977
Rain Staub <Rainstaub1@gmail.com>
2019-12-14 15:44:42 +0200

    Teine Task tehtud.

---
+++ tests/unit/Header.spec.js
@@ -0,0 +1,34 @@
+import { mount } from '@vue/test-utils'
+import Header from "../../src/components/Header.vue";
+
+describe('Header is rendered correctly', () => {
+    // Now mount the component and you have the wrapper
+    const wrapper = mount(Header)
+
+    // Check that this component properly displays today's date
+
+    it('Renders the correct date', () => {
+        let today = new Date();
+        let date = today.getDate() < 10 ?  `0${today.getDate()}` : today.getDate();
+        expect(wrapper.html()).toContain(date)
+    })
+	
+	it('Renders the correct day', () =>{
+		let today = new Date();
+		let weekday = today.toLocaleDateString('en-US', { weekday: 'long' });
+		expect(wrapper.html()).toContain(weekday.toUpperCase())
+	})
+	
+	it('Renders the correct year', () =>{
+		let today = new Date();
+		let year = today.toLocaleDateString('en-US', {year: 'numeric'});
+		expect(wrapper.html()).toContain(year)
+	})
+	
+	it('Renders the correct month', () =>{
+		let today = new Date();
+		let month = today.toLocaleDateString('en-US', {month: 'short'});
+		expect(wrapper.html()).toContain(month.toUpperCase())
+	})
+});
+
