d1313c667a83d3875fb3b51aa259733d34a971a5
Merlis Pajustik <merlis.pajustik@gmail.com>
2019-12-14 23:24:21 +0200

    lisasin esimesse ülesande testi kontrollimaks kas class list-item eksisteerib ja pärast seda kontrollib, kas html-s on vastav tekst olemas

--- tests/unit/List.spec.js
+++ tests/unit/List.spec.js
@@ -1,9 +1,10 @@
-import { mount} from '@vue/test-utils'
+import { mount } from "@vue/test-utils";
 import List from "../../src/components/List.vue";
 
-describe('List is rendered correctly', () => {
-    const wrapper = mount(List)
-    it('Add your first Todo task is displayed with no items', () => {
-        expect(wrapper.html()).toContain('Add your first Todo task')
-    })
+describe("List is rendered correctly", () => {
+  const wrapper = mount(List);
+  it("if no list item is provided List component displays text 'Add your first Todo task'", () => {
+    expect(wrapper.contains(".list-item")).toBe(false);
+    expect(wrapper.html()).toContain("Add your first Todo task");
+  });
 });
